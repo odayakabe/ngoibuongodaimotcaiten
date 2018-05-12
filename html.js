508fbbee786ff32f061a8c147ef8a1161890a55a
Odayaka <37972067+odayakabe@users.noreply.github.com>
2018-05-12 19:41:34 +0700

    7339845551120017 vietvirui

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-IDHERE',
+    google_ad_client: 'ca-pub-7339845551120017',
     enable_page_level_ads: true
   });
   `;
